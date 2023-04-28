e35df229f701e49641d85ec6a42b505c9a5eb117
dvyne10 <dagadu.divine@gmail.com>
2023-04-27 21:51:11 -0400

    chore: added shadows and tweaked ghosts

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -158,6 +158,8 @@ for (let i = 0; i <= 50; i++) {
   grave.position.set(x, 0.3, z);
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+
+  grave.castShadow = true;
   graves.add(grave);
 }
 
@@ -283,6 +285,24 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#262837");
 
 /**
+ * Shadows
+ */
+renderer.shadowMap.enabled = true;
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+floor.receiveShadow = true;
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
@@ -290,10 +310,21 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  const ghost1Angle = elapsedTime + 0.5;
+  const ghost1Angle = elapsedTime * 0.5;
   ghost1.position.x = Math.cos(ghost1Angle) * 4;
-  ghost1.position.y = Math.sin(ghost1Angle) * 4;
-  ghost1.position.z = Math.sin(elapsedTime * 3);
+  ghost1.position.z = Math.sin(ghost1Angle) * 4;
+  ghost1.position.y = Math.sin(elapsedTime * 3);
+
+  const ghost2Angle = -elapsedTime * 0.32;
+  ghost2.position.x = Math.cos(ghost2Angle) * 5;
+  ghost2.position.z = Math.sin(ghost2Angle) * 5;
+  ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
+
+  const ghost3Angle = -elapsedTime * 0.18;
+  ghost3.position.x =
+    Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5));
+  ghost3.position.y = Math.sin(elapsedTime * 5) + Math.sin(elapsedTime * 2);
 
   // Update controls
   controls.update();
